42244eac1fadddc9d37ddcb25b30b512c0634003
Ayush Patil <aayushpatilofficial@gmail.com>
2025-11-05 09:45:09 +0000

    yessss

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
-import webbrowser
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+# Use eventlet as async mode
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@socketio.on('command')
-def handle_command(data):
-    cmd = data['command'].strip().lower()
-    emit('message', {'msg': f'Executing command: {cmd}'}, broadcast=True)
+# Example SocketIO event
+@socketio.on("message")
+def handle_message(msg):
+    print("Message received:", msg)
+    socketio.send(f"You said: {msg}")
 
-    if cmd.startswith('open '):
-        url = cmd.split('open ')[1]
-        if not url.startswith(('http://', 'https://')):
-            url = 'https://' + url
-        webbrowser.open(url)
-        emit('message', {'msg': f'Opened {url}'}, broadcast=True)
-    else:
-        emit('message', {'msg': 'Unknown command.'}, broadcast=True)
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    socketio.run(app, host="0.0.0.0", port=port)
